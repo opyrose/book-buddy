71e80e433eddf87072e952cdcb44f5b76be57985
Your Name <you@example.com>
2024-11-10 21:41:50 -0500

    added register and login function, attempted ternary

--- book-buddy/src/App.jsx
+++ book-buddy/src/App.jsx
@@ -1,6 +1,7 @@
 import AllBooks from "./AllBooks"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom"
 import BookDetails from "./BookDetails"
+import Account from "./Account"
 
 
 const App = () => {
@@ -9,11 +10,17 @@ const App = () => {
   return (
     <>
   <h1>Book Buddy</h1>
+  <nav>
+    <Link to={'/'}>Home</Link>
+    <Link to={'books/29'}>Book of the Month</Link>
+    <Link to={'/account'}>Account</Link>
+  </nav>
 
     <Routes>
 
     <Route path = "/" element = {<AllBooks />}/>
     <Route path ="books/:id" element = {<BookDetails/>}/>
+    <Route path ="/account" element={<Account />}/>
 
     </Routes>
     </>
